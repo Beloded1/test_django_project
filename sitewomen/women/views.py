7ba7df6d691044f95dea1efece7a806f4c762071
Beloded1 <alexandrbeloded@gmail.com>
2024-02-06 15:46:39 +0400

    Finished 8 lesson

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
 def index(request):
@@ -17,6 +18,9 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('sport',))
+        return HttpResponsePermanentRedirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
